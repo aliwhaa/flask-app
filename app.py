ec97ad36bc51e1d6eba6d261198c4cb58ce89750
Ali <m.ali.pk04@outlook.com>
2025-08-28 16:53:31 +0500

    Added new feature

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route("/new")
+def about():
+    return render_template("new_feature.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
